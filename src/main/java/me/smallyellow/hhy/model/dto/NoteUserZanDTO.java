878921d62dd25c4c16e573d70f549a2667d9a89d
huanghuiyao <hhy107107@qq.com>
2017-12-03 22:07:54 +0800

    1

--- src/main/java/me/smallyellow/hhy/model/dto/NoteUserZanDTO.java
+++ src/main/java/me/smallyellow/hhy/model/dto/NoteUserZanDTO.java
@@ -2,6 +2,13 @@ package me.smallyellow.hhy.model.dto;
 
 import me.smallyellow.hhy.model.NoteUserZan;
 
+/**
+ * 用户赞文章情况，扩展了文章总攒书
+ * @author lenovo
+ *
+ */
 public class NoteUserZanDTO extends NoteUserZan{
+
+	private static final long serialVersionUID = -1375569955522070121L;
 
 }
